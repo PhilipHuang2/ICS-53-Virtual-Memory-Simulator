125f0aa16381aec92912050ca33057ee7b4ad6f5
Cyan0147 <Cyan0147@hotmail.com>
2020-02-06 20:49:31 -0800

    Function base
    Added the base of the first 5 functions definitions

--- main.c
+++ main.c
@@ -22,14 +22,52 @@ int main()
 	char input[90];
 	printf("Hello World\n");
 	do
-{
+	{
+
+	printf("> ");
 	fgets(input, 90, stdin);
 	printf("%s", input);
 	
 	
-}while(strcmp(input,"quit\n") != 0);
+	}while(strcmp(input,"quit\n") != 0);
 
 
 	return 0;
 
+}
+
+
+void read(int virtualAddress){
+
+	//prints contente of the Memory address
+
+	// if (){ //if page fault occurss print
+	// 	printf("An Page Fault Has Ocurred\n");
+	// }
+
+}
+
+void write (int virtualAddress, int num){
+
+	//writes data to a memory location
+
+	// if (){ //if page fault occurss print
+	// 	printf("An Page Fault Has Ocurred\n");
+	// }
+}
+
+void showMain (int physicalPageNumber){
+
+	//prints out the 4 address and data associated with the main memory page
+
+}
+
+void showDisk(int diskPageNumber){
+
+	//prints out the 4 addresses and data associated with the disk page
+
+}
+
+void showPageTable(){
+	//print out the 8 pageEntry Objects
 }
