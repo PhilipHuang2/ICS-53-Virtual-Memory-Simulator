544af922569bf45632047286bf015cd28a6da537
Philip Huang <philiphuang2@gmail.com>
2020-02-08 22:47:37 -0800

    Adding helper functions to use in the read and write statements

--- main.c
+++ main.c
@@ -23,10 +23,13 @@ void write (int virtualAddress, int num);
 void showMain (int physicalPageNumber);
 void showDisk(int diskPageNumber);
 void showPageTable();
+int findAvailablePage();
+void copyPageMemToDisk(int source, int destination);
+void copyPageDiskToMem(int source, int destination);
 
 struct pageEntry pageTable[8];
-int mainMemory[16]= {0};
-int disk[32] = {0};	
+int mainMemory[16]= {-1}; // need to initialize all values to negative 1
+int disk[32] = {-1};	// same here
 int main()
 {
 	char input[90];
@@ -89,10 +92,80 @@ int main()
 	}while(strcmp(input,"quit") != 0);
 }
 
-void read(int virtualAddress){
+void copyPageMemToDisk(int source, int destination)
+{
+	int increment = 0;
+	int firstDiskAddress = destination * 4;
+	int firstMainMemAddress = source *4;
+	while( increment < 4)
+	{
+		disk[firstDiskAddress + increment] 
+			= mainMemory[firstMainMemAddress + increment];
+	}
+}
+void copyPageDiskToMem(int source, int destination)
+{
+	int increment = 0;
+	int firstDiskAddress = source * 4;
+	int firstMainMemAddress = destination *4;
+	while( increment < 4)
+	{
+		mainMemory[firstMainMemAddress + increment] =
+			disk[firstDiskAddress + increment];
+	}
+}
 
+
+int findAvailablePage()
+{
+	//returns the first avaiable page in main memory that is free [0-3]
+	// otherwises return -1 if no space is available.
+	int mark[4]; // all should be initialized to 0;
+	int count = 0;
+	while(count <= 7)
+	{
+		if(pageTable[count].valid == 1)
+		{
+			mark[pageTable[count].pageNum] = -1;
+		}
+		count++;
+	}
+	count = 0;
+	while(count <= 3)
+	{
+		if(mark[count] == 0)
+			return count;
+		count++;
+	}
+	return -1;
+
+}
+
+void read(int virtualAddress){
 	//prints content of the Memory address
-	printf("reading function, read this address %d \n",virtualAddress);
+	int virtualPage = virtualAddress/4;
+	printf("reading function, read this address %d\n",virtualPage);
+	if(pageTable[virtualPage].valid != 1)
+	{
+		printf("An Page Fault Has Ocurred\n");
+		showPageTable();
+		// printf("Virtual Page %d is not in main memory\n", virtualPage);
+		// printf("Physical Page %d in Main Memory is open.\n", findAvailablePage());
+		int availablePage = findAvailablePage();
+		if(availablePage == -1)
+		{
+			//FindVictimPage
+			//CopyPage if dirty
+			//CopyPage
+		}
+		copyPageDiskToMem(virtualPage,availablePage);
+		return;
+	}
+		
+	// pageTable[virtualPage].pageNum = page in Memory
+	// virtualAddress % 4 =  offset from the 0th address in page;
+	printf("This is the physical Address: %d\n", pageTable[virtualPage].pageNum + virtualAddress % 4);
+	
 	// if (){ //if page fault occurss print
 	// 	printf("An Page Fault Has Ocurred\n");
 	// }
@@ -110,6 +183,11 @@ printf("writing function, write in this address %d, this num %d\n",virtualAddres
 
 void showMain (int physicalPageNumber){
 	//prints out the 4 address and data associated with the main memory page
+	if(physicalPageNumber < 0 || physicalPageNumber > 3)
+	{
+		printf("Page %d does not exist in Main Memory, Trying searching pages 0-3.\n", physicalPageNumber);
+		return;
+	}
 	int count = physicalPageNumber * 4;
 	int final = count + 4;
 	while(count < final)
@@ -121,6 +199,11 @@ void showMain (int physicalPageNumber){
 
 void showDisk(int diskPageNumber){
 	//prints out the 4 addresses and data associated with the disk page
+	if(diskPageNumber < 0 || diskPageNumber > 7)
+	{
+		printf("Page %d does not exist in Main Memory, Trying searching pages 0-7.\n", diskPageNumber);
+		return;
+	}
 	int count = diskPageNumber * 4;
 	int final = count + 4;
 	while(count < final)
